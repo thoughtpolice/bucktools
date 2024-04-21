9759bec3e550b21d1e5471a07d2b64d80898b043
Austin Seipp <aseipp@pobox.com>
2024-04-21 00:38:38 -0500

    nix: use mold, zstd debuginfo, and parallel rustc frontend

--- flake.nix
+++ flake.nix
@@ -18,15 +18,47 @@
         overlays = [ (import rust-overlay) ];
       };
 
-    my-rust-bin = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.complete);
+      ourRustVersion = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.complete);
+
+      # these are needed in both devShell and buildInputs
+      darwinDeps = with pkgs; lib.optionals stdenv.isDarwin [
+        darwin.apple_sdk.frameworks.Security
+        darwin.apple_sdk.frameworks.SystemConfiguration
+        libiconv
+      ];
+
+      # work around https://github.com/nextest-rs/nextest/issues/267
+      # this needs to exist in both the devShell and preCheck phase!
+      darwinNextestHack = pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
+        export DYLD_FALLBACK_LIBRARY_PATH=$(${ourRustVersion}/bin/rustc --print sysroot)/lib
+      '';
+
+      # NOTE (aseipp): on Linux, go ahead and use mold by default to improve
+      # link times a bit; mostly useful for debug build speed, but will help
+      # over time if we ever get more dependencies, too
+      useMoldLinker = pkgs.stdenv.isLinux;
+
+      # these are needed in both devShell and buildInputs
+      linuxNativeDeps = with pkgs; lib.optionals stdenv.isLinux [
+        mold-wrapped
+      ];
 
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
-          cargo-bloat protobuf my-rust-bin
+          ourRustVersion
+
+          cargo-bloat protobuf
           grpcurl tokio-console sqlite
           pkg-config fuse
-        ];
+        ] ++ darwinDeps ++ linuxNativeDeps;
+        shellHook = ''
+          export RUST_BACKTRACE=1
+        '' + pkgs.lib.optionalString useMoldLinker ''
+          export RUSTFLAGS="--cfg tokio_unstable -C link-arg=-fuse-ld=mold"
+        '' + ''
+          export RUSTFLAGS="-Zthreads=0 $RUSTFLAGS -C link-arg=-Wl,--compress-debug-sections=zstd"
+        '' + darwinNextestHack;
       };
     });
 }
