1bebdf612b478b92bd7bd88ec55f17658aa909fb
Austin Seipp <aseipp@pobox.com>
2024-07-11 20:36:13 +0000

    devcontainer: initialize jj repo on setup
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+$PWD/tools/bin/jj git init --colocate || true
+$PWD/tools/bin/jj config set --user user.name  "$(git config --system user.name)"
+$PWD/tools/bin/jj config set --user user.email "$(git config --system user.email)"
+$PWD/tools/bin/jj branch track main@origin
