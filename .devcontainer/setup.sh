944bb426cf2bcb14028489ee289d7cb6d19dd161
Austin Seipp <aseipp@pobox.com>
2024-07-12 09:52:40 +0000

    devcontainer: also set default ui.default-command=log
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 $PWD/tools/bin/jj config set --user user.name  "$(git config --system user.name)"
 $PWD/tools/bin/jj config set --user user.email "$(git config --system user.email)"
+$PWD/tools/bin/jj config set --user ui.default-command log
+
 $PWD/tools/bin/jj git init --colocate || true
 $PWD/tools/bin/jj branch track main@origin
